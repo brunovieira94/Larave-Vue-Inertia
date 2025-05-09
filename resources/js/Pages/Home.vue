27ce9e3503aba967534f2de521ac628f0e6b5e42
Bruno Ramos <bruno.ramos@in8.com.br>
2025-05-09 01:30:40 -0300

    Project Setup

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="text-3xl font-bold text-center my-8 bg-white">Welcome to the Home Page</h1>
+        <p class="text-center">This is a simple Vue.js application.</p>
+    </div>
+</template>
